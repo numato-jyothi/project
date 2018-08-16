b8ae44089b2b1ed3e63ea70f5309741e602e8cc3
numato-jyothi <jyothi.as@numato.com>
2018-08-16 12:52:41 +0530

    notgate code

---
+++ notgate_sim.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    09:27:18 06/19/2018 
+// Design Name: 
+// Module Name:    notgate_sim 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module notgate(a, b);
+    input wire a;
+    output wire b;
+  assign b = !a ;
+
+
+endmodule
